98d9687785db9f400fa9f71148dbe0674bd476aa
TRUONG MINH TUAN <tieudinhtuan@gmail.com>
2023-04-18 10:16:11 +0700

    Add files via upload

---
+++ learnX/Infrastructure/EF/EXDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.EF
+{
+    public class EXDbContext : DbContext
+    {
+        public EXDbContext(DbContextOptions<EXDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> user { get; set; }
+    }
+}
